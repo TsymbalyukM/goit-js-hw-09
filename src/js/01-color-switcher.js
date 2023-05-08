5f8b80d1281ac26259c1789da026ca1b80100dd0
TsymbalyukM <mashatsymbaliuk2@gmail.com>
2023-05-09 01:06:44 +0300

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = '';
+
+stopBtn.setAttribute('disabled', 'true');
+
+startBtn.addEventListener('click', handleStartClick);
+stopBtn.addEventListener('click', handleStopClick);
+
+function handleStartClick() {
+  intervalId = setInterval(() => {
+    changeColor();
+  }, 1000);
+
+  startBtn.setAttribute('disabled', 'true');
+  stopBtn.removeAttribute('disabled');
+}
+
+function handleStopClick() {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'true');
+}
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
