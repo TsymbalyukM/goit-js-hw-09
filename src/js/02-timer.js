26d51216125d32fecb1e9d25182b4d0de8f1c38a
TsymbalyukM <mashatsymbaliuk2@gmail.com>
2023-05-19 11:24:55 +0300

    fix 2 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const buttonStart = document.querySelector('[data-start]');
 const daysRef = document.querySelector('[data-days]');
@@ -7,24 +8,19 @@ const hoursRef = document.querySelector('[data-hours]');
 const minutesRef = document.querySelector('[data-minutes]');
 const secondsRef = document.querySelector('[data-seconds]');
 
-let intervalId = 'null';
+let intervalId = null;
 
 buttonStart.setAttribute('disabled', true);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -38,9 +34,8 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
     if (selectedDates[0] < new Date()) {
-      window.alert('Please choose a date in the future');
+      Notiflix.Notify.failure('Please choose a date in the future');
       buttonStart.setAttribute('disabled', true);
       return;
     }
@@ -52,12 +47,14 @@ const options = {
       const timeDifference = targetTime - now;
 
       if (timeDifference <= 0) {
-        clearInterval(intervalId);
+        clearInterval(countdownInterval);
         daysRef.textContent = '0';
         hoursRef.textContent = '00';
         minutesRef.textContent = '00';
         secondsRef.textContent = '00';
-        buttonStart.setAttribute('disabled', true);
+        startBtn.setAttribute('disabled', true);
+
+        return;
       }
 
       const { days, hours, minutes, seconds } = convertMs(timeDifference);
@@ -78,4 +75,5 @@ const options = {
     buttonStart.addEventListener('click', onClick);
   },
 };
+
 flatpickr('#datetime-picker', { ...options });
